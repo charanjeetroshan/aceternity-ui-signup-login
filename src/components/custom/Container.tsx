f13213403aedeb46f45538635e9553039b03deac
Charanjeet <singhcharanjeet603@gmail.com>
2024-07-19 20:27:43 +0200

    Make children prop optional for container component

--- src/components/custom/Container.tsx
+++ src/components/custom/Container.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 type StackProps = {
-   children: React.ReactNode;
+   children?: React.ReactNode;
    className?: string;
 };
 
 function Container(props: StackProps) {
    return (
       <div
-         className={`flex flex-col items-center justify-center w-full min-h-screen p-4 md:p-8 ${props.className}`}
-      >
+         className={`flex flex-col items-center justify-center w-full min-h-screen p-4 md:p-8 ${props.className}`}>
          {props.children}
       </div>
    );
