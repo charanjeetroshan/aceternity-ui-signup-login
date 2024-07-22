fdfffa83da012209adc46878975fe8c280efae1e
Charanjeet <singhcharanjeet603@gmail.com>
2024-07-22 19:52:49 +0200

    Implement chunking during application build and set up build rollup visualizer plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react(), tsconfigPaths()],
+   plugins: [react(), tsconfigPaths(), visualizer()],
    resolve: {
       alias: {
          "@": "/src",
@@ -12,5 +13,32 @@ export default defineConfig({
    },
    server: {
       open: true,
+   },
+   build: {
+      rollupOptions: {
+         output: {
+            manualChunks(id) {
+               if (id.includes("react-hook-form")) {
+                  return "react-hook-form";
+               }
+
+               if (id.includes("zod")) {
+                  return "zod";
+               }
+
+               if (id.includes("axios")) {
+                  return "axios";
+               }
+
+               if (id.includes("framer-motion")) {
+                  return "framer-motion";
+               }
+
+               if (id.includes("react-dom")) {
+                  return "react-dom";
+               }
+            },
+         },
+      },
    },
 });
